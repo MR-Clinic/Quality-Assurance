0aa64e30de1d69b9c7d2ac78620ba390aed4152b
Rofik Aw <rofikawaludin99@gmail.com>
2022-03-12 08:00:22 +0700

    first commit

--- src/test/java/starter/postcodes/StepDefintion/loginStepdefs.java
+++ src/test/java/starter/postcodes/StepDefintion/loginStepdefs.java
@@ -1,2 +1,25 @@
-package starter.postcodes.StepDefintion;public class loginStepdefs {
+package starter.postcodes.StepDefintion;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
+import starter.postcodes.step.Login;
+
+public class loginStepdefs {
+
+    @Steps
+    Login login;
+
+   @When("user login with username {string} and password is {string}")
+    public void userlogin(String userName, String password){
+       login.postUser(userName, password);
+    }
+
+   @Then("user success login")
+    public void usersucceslogin(){
+       SerenityRest.then()
+               .statusCode(200);
+   }
+
 }
--- src/test/resources/features/postcodes/RegresAPI.feature
+++ src/test/resources/features/postcodes/RegresAPI.feature
@@ -3,6 +3,11 @@ Feature: Reqres API
 
   @createUser
   Scenario: Create user
-    When I create new User with name is "Fifeka" and job is "Quality Assurance"
+    When I create new User with name is "RofikAw" and job is "Quality Assurance"
     Then New user is created
-    And New user name is "Fifeka" and job is "Quality Assurance"
+    And New user name is "RofikAw" and job is "Quality Assurance"
+
+  @loginmrclinic
+  Scenario: login mr clinic
+    When user login with username "userName" and password is "password"
+    Then user success login
